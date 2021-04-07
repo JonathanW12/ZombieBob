9ba9915128d1b9c018e86f4c6d9f74a0d095a4a0
Nicolai Krogager Asmussen <niasm19@student.sdu.dk>
2021-04-07 14:01:22 +0200

    added javadoc to parts

---
+++ zombie/Common/src/main/java/dk/sdu/mmmi/cbse/common/data/entitytypeparts/BulletAmmoPart.java
@@ -0,0 +1,10 @@
+package dk.sdu.mmmi.cbse.common.data.entitytypeparts;
+
+import dk.sdu.mmmi.cbse.common.data.entityparts.EntityPart;
+
+/**
+ * Used to identify ammonition of type bullet
+ */
+
+public class BulletAmmoPart implements EntityPart {
+}
