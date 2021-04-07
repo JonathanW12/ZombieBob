6c31de0a3563716189a0dbfa169b401b3546792c
Nicolai Krogager Asmussen <niasm19@student.sdu.dk>
2021-04-07 12:21:01 +0200

    readded project

---
+++ zombie/Common/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/MovingPart.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.common.data.entityparts;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+
+public class MovingPart
+        implements EntityPart {
+
+    private float dx, dy;
+    private float deceleration, acceleration;
+    private float maxSpeed, rotationSpeed;
+    private boolean left, right, up, down;
+
+    public MovingPart(float deceleration, float acceleration, float maxSpeed, float rotationSpeed) {
+        this.deceleration = deceleration;
+        this.acceleration = acceleration;
+        this.maxSpeed = maxSpeed;
+        this.rotationSpeed = rotationSpeed;
+    }
+
+    public float getDx() {
+        return dx;
+    }
+
+    public float getDy() {
+        return dy;
+    }
+
+    public void setDx(float dx){
+        this.dx = dx;
+    }
+
+    public void setDy(float dy){
+        this.dy = dy;
+    }
+
+    public void setDeceleration(float deceleration) {
+        this.deceleration = deceleration;
+    }
+
+    public void setAcceleration(float acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public void setMaxSpeed(float maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setSpeed(float speed) {
+        this.acceleration = speed;
+        this.maxSpeed = speed;
+    }
+
+
+
+    public void setRotationSpeed(float rotationSpeed) {
+        this.rotationSpeed = rotationSpeed;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public float getDeceleration() {
+        return deceleration;
+    }
+
+    public float getAcceleration() {
+        return acceleration;
+    }
+
+    public float getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public float getRotationSpeed() {
+        return rotationSpeed;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    @Override
+    public void process(GameData gameData, EntityPart entityPart) {
+
+    }
+
+}
---
+++ zombie/Common/src/main/java/dk/sdu/mmmi/cbse/common/data/entitytypeparts/PlayerPart.java
@@ -0,0 +1,11 @@
+package dk.sdu.mmmi.cbse.common.data.entitytypeparts;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.entityparts.EntityPart;
+
+public class PlayerPart implements EntityPart {
+    @Override
+    public void process(GameData gameData, EntityPart entityPart) {
+
+    }
+}
