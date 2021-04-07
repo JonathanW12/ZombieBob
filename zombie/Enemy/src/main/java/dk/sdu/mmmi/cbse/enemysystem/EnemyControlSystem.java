c7a1849a849b5661d8e2293a54b548422ff02e47
LakeExhaust <45452219+ZeroExhaust@users.noreply.github.com>
2021-04-07 15:20:04 +0200

    Enemy
    Er tilføjet men dens movement er scuffed

---
+++ zombie/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemysystem/EnemyControlSystem.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.enemysystem;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.GameKeys;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.entityparts.EntityPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.data.entityparts.PositionPart;
+import dk.sdu.mmmi.cbse.common.data.entitytypeparts.VisualPart;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import java.util.Map;
+import java.util.Random;
+import java.util.UUID;
+import org.openide.util.lookup.ServiceProvider;
+import org.openide.util.lookup.ServiceProviders;
+
+/**
+ *
+ * @author lake
+ */
+
+@ServiceProviders(value = {
+    @ServiceProvider(service = IEntityProcessingService.class)})
+public class EnemyControlSystem implements IEntityProcessingService {
+ Random randomGenerator = new Random();
+ int randomNumb = randomGenerator.nextInt(3);
+    @Override
+    public void process(GameData gd, World world) {
+          // all playerParts
+        for (Map.Entry<UUID,EntityPart> entry : world.getMapByPart("EnemyPart").entrySet()){
+
+            // entity parts on enemy
+            PositionPart positionPart = (PositionPart) world.getMapByPart("PositionPart").get(entry.getKey());
+            MovingPart movingPart = (MovingPart) world.getMapByPart("MovingPart").get(entry.getKey());
+            VisualPart visualPart = (VisualPart) world.getMapByPart("VisualPart").get(entry.getKey());
+
+            // movement
+            switch(randomNumb) {       
+            case 1:
+            movingPart.setLeft(true);
+            break;
+            case 2:
+            movingPart.setRight(true);
+            break;
+            case 3:
+            movingPart.setUp(true);
+            break;
+            case 4:
+            movingPart.setDown(true);
+             }
+            // update
+            updateShape(visualPart, positionPart);
+            }
+    }
+     private void updateShape(VisualPart entity, PositionPart position) {
+        float[] shapex = new float[4];
+        float[] shapey = new float[4];
+        PositionPart positionPart = position;
+        float x = positionPart.getX();
+        float y = positionPart.getY();
+        float radians = positionPart.getRadians();
+
+        shapex[0] = (float) (x + Math.cos(radians) * entity.getRadius());
+        shapey[0] = (float) (y + Math.sin(radians) * entity.getRadius());
+
+        shapex[1] = (float) (x + Math.cos(radians - 4 * 3.1415f / 5) * entity.getRadius());
+        shapey[1] = (float) (y + Math.sin(radians - 4 * 3.1145f / 5) * entity.getRadius());
+
+        shapex[2] = (float) (x + Math.cos(radians + 3.1415f) * entity.getRadius() * 0.5);
+        shapey[2] = (float) (y + Math.sin(radians + 3.1415f) * entity.getRadius() * 0.5);
+
+        shapex[3] = (float) (x + Math.cos(radians + 4 * 3.1415f / 5) * entity.getRadius());
+        shapey[3] = (float) (y + Math.sin(radians + 4 * 3.1415f / 5) * entity.getRadius());
+
+        entity.setShapeX(shapex);
+        entity.setShapeY(shapey);
+    } 
+  
+}
