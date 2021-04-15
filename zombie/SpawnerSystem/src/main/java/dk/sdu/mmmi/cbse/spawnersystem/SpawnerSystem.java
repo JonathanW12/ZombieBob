f8357c22c80b907b3c800c5fcb665b28e3ea4803
Tobias Kristensen <tobiaskristensen12@gmail.com>
2021-04-15 14:07:24 +0200

    Spawner prototype

---
+++ zombie/SpawnerSystem/src/main/java/dk/sdu/mmmi/cbse/spawnersystem/SpawnerSystem.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.spawnersystem;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import org.openide.util.lookup.ServiceProvider;
+
+/**
+ *
+ * @author tobia
+ */
+
+@ServiceProvider(service = IEntityProcessingService.class)
+public class SpawnerSystem implements IEntityProcessingService {
+    
+    // private EnemyPlugin enemyPlugin = EnemyPlugin.getInstance();
+    private long currentTime = System.currentTimeMillis();
+    private long lastSpawnTime = currentTime;
+    
+    
+    @Override
+    public void process(GameData gameData, World world) {
+        if (lastSpawnTime < currentTime - 5000) {
+            lastSpawnTime = currentTime;
+            // enemyPlugin.spawn();
+        }
+        
+        updateTime();
+    }
+    
+    private void updateTime() {
+        currentTime = System.currentTimeMillis();
+    }
+}
