081b73f8ed660208cda16b5d773991ee51911148
Tobias <tokri19@student.sdu.dk>
2021-04-21 14:44:31 +0200

    Add spider enemy

--- zombie/StandardZombie/src/main/java/dk/sdu/mmmi/cbse/standardZombieSystem/ZombieControlSystem.java
+++ zombie/StandardZombie/src/main/java/dk/sdu/mmmi/cbse/standardZombieSystem/ZombieControlSystem.java
@@ -1,17 +1,12 @@
 package dk.sdu.mmmi.cbse.standardZombieSystem;
 
-import dk.sdu.mmmi.cbse.common.data.Entity;
-import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
-import dk.sdu.mmmi.cbse.common.data.GameKeys;
 import dk.sdu.mmmi.cbse.common.data.World;
-import dk.sdu.mmmi.cbse.common.data.entityparts.AnimationPart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.CombatPart;
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.entityparts.*;
 import dk.sdu.mmmi.cbse.common.data.entitytypeparts.EnemyPart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.WeaponInventoryPart;
-import dk.sdu.mmmi.cbse.common.data.entityparts.WeaponPart;
 import dk.sdu.mmmi.cbse.common.data.entityparts.VisualPart;
+import dk.sdu.mmmi.cbse.common.data.entitytypeparts.PlayerPart;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import org.openide.util.lookup.ServiceProvider;
 import org.openide.util.lookup.ServiceProviders;
@@ -21,18 +16,71 @@ import java.util.UUID;
 @ServiceProviders(value = {
     @ServiceProvider(service = IEntityProcessingService.class)})
 public class ZombieControlSystem implements IEntityProcessingService {
+    
+    private final long spawnDelay = 5000;
+    private long currentTime = System.currentTimeMillis();
+    private long lastSpawnTime = currentTime;
 
     @Override
     public void process(GameData gameData, World world) {
-        for (Map.Entry<UUID,EntityPart> entry : world.getMapByPart(EnemyPart.class.getSimpleName()).entrySet()){
+        if (world.getMapByPart(EnemyPart.class.getSimpleName()) != null) {
+            for (Map.Entry<UUID,EntityPart> entry : world.getMapByPart(EnemyPart.class.getSimpleName()).entrySet()){
             
-            PositionPart positionPart = (PositionPart) world.getMapByPart("PositionPart").get(entry.getKey());
-            MovingPart movingPart = (MovingPart) world.getMapByPart("MovingPart").get(entry.getKey());
-            VisualPart visualPart = (VisualPart) world.getMapByPart("VisualPart").get(entry.getKey());
-            
-            //Movement handled by ai part i believe
-            //Animations to be added
+                PositionPart positionPart = (PositionPart) world.getMapByPart("PositionPart").get(entry.getKey());
+                MovingPart movingPart = (MovingPart) world.getMapByPart("MovingPart").get(entry.getKey());
+                VisualPart visualPart = (VisualPart) world.getMapByPart("VisualPart").get(entry.getKey());
+                AnimationPart animationPart = (AnimationPart) world.getMapByPart(AnimationPart.class.getSimpleName()).get(entry.getKey());
+                CombatPart combatPart = (CombatPart) world.getMapByPart(CombatPart.class.getSimpleName()).get(entry.getKey());
+                
+                attackPlayer(gameData, world, entry.getKey());
+
+                // Animation processing
+                if (!animationPart.isCurrentAnimationInterruptible() && !animationPart.hasCurrentAnimationLooped()) {
+                    animationPart.setIsAnimated(true);
+                } else {
+                    animationPart.setIsAnimated(
+                        (movingPart.isDown() || movingPart.isLeft() || movingPart.isRight() || movingPart.isUp() || combatPart.isAttacking())
+                    );
+                }
+
+                visualPart.setSpriteName("spiderIdle");
+                if (movingPart.isDown() || movingPart.isLeft() || movingPart.isRight() || movingPart.isUp()) {
+                    animationPart.setCurrentAnimation("walk");
+                }
+            }
+        }
+        
+        spawnZombies(gameData, world);
+    }
+    
+    private void attackPlayer(GameData gameData, World world, UUID zombieID) {
+        PositionPart zombiePos = (PositionPart) world.getMapByPart(PositionPart.class.getSimpleName()).get(zombieID);
+        CombatPart combatPart = (CombatPart) world.getMapByPart(CombatPart.class.getSimpleName()).get(zombieID);
+        
+        if (world.getMapByPart(PlayerPart.class.getSimpleName()) != null) {
+            for (Map.Entry<UUID,EntityPart> player : world.getMapByPart(EnemyPart.class.getSimpleName()).entrySet()) {
+                PositionPart playerPos = (PositionPart) world.getMapByPart(PositionPart.class.getSimpleName()).get(player.getKey());
+                float distance = (float) Math.sqrt(
+                    Math.pow(zombiePos.getX() - playerPos.getX(), 2) +
+                    Math.pow(zombiePos.getY() - playerPos.getY(), 2)
+                );
+                
+                if (distance <= 5) {
+                    combatPart.setAttacking(true);
+                } 
+            }
         }
     }
     
+    private void spawnZombies(GameData gameData, World world) {
+        currentTime = System.currentTimeMillis();
+        if (currentTime > lastSpawnTime + spawnDelay) {
+            float spawnX = gameData.getDisplayWidth() - 25;
+            float spawnY = gameData.getDisplayHeight() / 2;
+            
+            ZombiePlugin.createZombie(gameData, world, spawnX, spawnY);
+            lastSpawnTime = currentTime;
+        }
+    }
+            
 }
--- zombie/StandardZombie/src/main/java/dk/sdu/mmmi/cbse/standardZombieSystem/ZombiePlugin.java
+++ zombie/StandardZombie/src/main/java/dk/sdu/mmmi/cbse/standardZombieSystem/ZombiePlugin.java
@@ -16,7 +16,7 @@ public class ZombiePlugin implements IGamePluginService{
 
     @Override
     public void start(GameData gameData, World world) {
-        createZombie(gameData, world);
+        // Do nothing
     }
     @Override
     public void stop(GameData gameData, World world) {
@@ -25,22 +25,54 @@ public class ZombiePlugin implements IGamePluginService{
         }
     }
     
-    private void createZombie(GameData gameData, World world){
+    protected static void createZombie(GameData gameData, World world, float x, float y){
         float speed = 2;
         float radians = 3.1415f / 2;
-        float x = 300;
-        float y = 400;
         float rotationSpeed = 3;
         
         Entity zombie = new Entity();
         
-        world.addtoEntityPartMap(new VisualPart("sword_sprite",80,80), zombie);
-        world.addtoEntityPartMap(new MovingPart(speed,rotationSpeed), zombie);
-        world.addtoEntityPartMap(new PositionPart(x,y,radians), zombie);
+        // Standard zombie attack
+        Entity zombieAttack = new Entity();
+        
+        WeaponPart weaponPart = new WeaponPart(
+            30,
+            5,
+            1f,
+            1
+        );
+        
+        world.addtoEntityPartMap(weaponPart, zombieAttack);
+        
+        CombatPart combatPart = new CombatPart();
+        combatPart.setCurrentWeapon(zombieAttack.getUUID());
+        
+        // Create zombie 
+        world.addtoEntityPartMap(new PositionPart(x, y, radians), zombie);
+        world.addtoEntityPartMap(new VisualPart("spiderIdle", 80, 80), zombie);
+        world.addtoEntityPartMap(new MovingPart(speed, rotationSpeed), zombie);
+        world.addtoEntityPartMap(new EnemyPart(), zombie);
         world.addtoEntityPartMap(new LifePart(100), zombie);
         world.addtoEntityPartMap(new AiMovementPart(4), zombie);
-        world.addtoEntityPartMap(new EnemyPart(), zombie);
-        //Animation to be added
-       
+        world.addtoEntityPartMap(new ColliderPart(40),zombie);
+        world.addtoEntityPartMap(combatPart, zombie);
+        world.addtoEntityPartMap(createInitialAnimation(), zombie);
+    }
+    
+    private static AnimationPart createInitialAnimation() {
+        AnimationPart animationPart = new AnimationPart();
+        animationPart.addAnimation(
+            "walk", // Set animation Id
+            "spiderWalk", // Animation asset name
+            4, // Frame count
+            0.15f, // Frame duration
+            true // Animation can be interrupted
+        );
+        animationPart.setCurrentAnimation("walk");
+        
+        // Increment loop counter to avoid walk animation on start
+        animationPart.getCurrentAnimation().setLoopCounter(2);
+        
+        return animationPart;
     }
 }
