527496daa5abd065d0e952e69666fe73be9e9f8a
Philip Rønnest <philip.eckhoff.r.nnest@gmail.com>
2021-04-21 12:29:33 +0200

    Added test for collision

--- zombie/CollisionSystem/src/main/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetectionProcessor.java
+++ zombie/CollisionSystem/src/main/java/dk/sdu/mmmi/cbse/collisionsystem/CollisionDetectionProcessor.java
@@ -32,6 +32,7 @@ public class CollisionDetectionProcessor implements IEntityProcessingService {
             collider1.getCollidingEntities().clear();
             
             PositionPart collider1Pos = ((PositionPart)world.getMapByPart("PositionPart").get(entry.getKey()));
+            
             if(collider1Pos != null){
                 if(collider1.getRadius() != 0){
                     for (Map.Entry<UUID, EntityPart> entry2: world.getMapByPart("ColliderPart").entrySet()) {
@@ -39,7 +40,6 @@ public class CollisionDetectionProcessor implements IEntityProcessingService {
                         if(entry.getKey() != entry2.getKey()){
                             ColliderPart collider2 = ((ColliderPart)entry.getValue());
                             PositionPart collider2Pos = ((PositionPart)world.getMapByPart("PositionPart").get(entry2.getKey()));
-                            
                             float distance = (float)Math.sqrt(Math.pow(collider1Pos.getX() - collider2Pos.getX(),2) + Math.pow(collider1Pos.getY() - collider2Pos.getY(),2));
                             if(distance < collider1.getRadius() + collider2.getRadius()){
                                 collider1.getCollidingEntities().add(entry2.getKey());
@@ -48,14 +48,10 @@ public class CollisionDetectionProcessor implements IEntityProcessingService {
                     }
                 
                 }else if(collider1.getWidth() != 0 && collider1.getHeight() != 0){
-                
+                    //check for box collision
                 }
             }
-            if(!collider1.getCollidingEntities().isEmpty()){
-                System.out.println(collider1.getCollidingEntities().toString());
-            }
         }
-    
     }
-    
 }
+
